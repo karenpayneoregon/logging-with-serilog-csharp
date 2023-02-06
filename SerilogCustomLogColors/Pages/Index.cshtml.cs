0fa1bb0c2c3a5dc54ef7032d810b8cefcf3ba3ea
Karen Payne <paynekaren@comcast.net>
2023-02-06 09:05:48 -0800

    new handler

--- SerilogCustomLogColors/Pages/Index.cshtml.cs
+++ SerilogCustomLogColors/Pages/Index.cshtml.cs
@@ -44,22 +44,33 @@ public class IndexModel : PageModel
         Log.Information("Is {day} a weekend day? {IsWeekday} ",DateTime.Today.DayOfWeek,  DateTime.Now.IsWeekDay());
         return new RedirectToPageResult("Index");
     }
-
+    #region handlers 
     public override void OnPageHandlerSelected(PageHandlerSelectedContext context)
     {
         if (context.HandlerMethod!.MethodInfo.Name == nameof(OnGet))
         {
             // page
-        }else if (context.HandlerMethod!.MethodInfo.Name == nameof(OnPostButton1))
+        }
+        else if (context.HandlerMethod!.MethodInfo.Name == nameof(OnPostButton1))
         {
             Log.Information($"Entering {nameof(OnPageHandlerSelected)} for {nameof(OnPostButton1)}");
         }
     }
-
 
     public override Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
     {
         Log.Information($"Entering {nameof(OnPageHandlerExecutionAsync)} for {context.HandlerMethod!.MethodInfo.Name}");
         return base.OnPageHandlerExecutionAsync(context, next);
     }
+
+    public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+        if (context.HandlerMethod!.MethodInfo.Name == nameof(OnPostButton4))
+        {
+            Log.Information("Redirecting");
+            context.Result = new RedirectResult("OtherPage");
+        }
+        base.OnPageHandlerExecuting(context);
+    }
+    #endregion
 }
