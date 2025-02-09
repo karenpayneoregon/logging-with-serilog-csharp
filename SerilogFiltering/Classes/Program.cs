41488135b2ba904071bfb94f94d23629ec0f7db2
Karen Payne <paynekaren@comcast.net>
2025-02-09 14:23:18 -0800

    Update console title to "Serilog Code sample"
    Changed the console window title in the Init method of the Program class from "Code sample" to "Serilog Code sample" to reflect the use of Serilog in the application.
    
    by Karen

--- SerilogFiltering/Classes/Program.cs
+++ SerilogFiltering/Classes/Program.cs
@@ -30,7 +30,7 @@ internal partial class Program
     public static void Init()
     {
         SetupLogging.Development();
-        Console.Title = "Code sample";
+        Console.Title = "Serilog Code sample";
         WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center);
     }
 
