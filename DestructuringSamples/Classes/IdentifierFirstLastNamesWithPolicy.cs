bb0557be0b21f37e2627576852ddfaecf73aedfa
Karen Payne <paynekaren@comcast.net>
2025-07-04 12:09:59 -0700

    Add custom destructuring policies for ICustomer logging
    This commit introduces two new classes: `FirstLastNamesWithPhonePolicy` and `IdentifierFirstLastNamesWithPolicy`, which implement the `IDestructuringPolicy` interface for enhanced logging of `ICustomer` objects in Serilog. The `FirstLastNamesWithPhonePolicy` class captures the `FirstName`, `LastName`, and `OfficePhoneNumber`, while the `IdentifierFirstLastNamesWithPolicy` class captures the `Id`, `FirstName`, and `LastName`. Additionally, the project file has been updated to include a new project for `IDestructuringPolicy`, and references to the previous classes have been removed from `CustomerDestructuringPolicy.cs`.
    
    by Karen

--- DestructuringSamples/Classes/IdentifierFirstLastNamesWithPolicy.cs
+++ DestructuringSamples/Classes/IdentifierFirstLastNamesWithPolicy.cs
@@ -8,28 +8,6 @@ namespace DestructuringSamples.Classes;
 /// Implements a custom destructuring policy for handling objects of type <see cref="ICustomer"/>.
 /// </summary>
 /// <remarks>
-/// This class is used to customize the way <see cref="ICustomer"/> objects are logged by Serilog.
-/// It extracts specific properties such as <c>FirstName</c>, <c>LastName</c>, and <c>OfficePhoneNumber</c> for logging purposes.
-/// </remarks>
-public class FirstLastNamesWithPhonePolicy : IDestructuringPolicy
-{
-    public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
-    {
-        if (value is ICustomer c)
-        {
-            result = propertyValueFactory.CreatePropertyValue(new { c.FirstName, c.LastName, c.OfficePhoneNumber });
-            return true;
-        }
-
-        result = null;
-        return false;
-    }
-}
-
-/// <summary>
-/// Implements a custom destructuring policy for handling objects of type <see cref="ICustomer"/>.
-/// </summary>
-/// <remarks>
 /// This class customizes the way <see cref="ICustomer"/> objects are logged by Serilog.
 /// It extracts specific properties such as <c>Id</c>, <c>FirstName</c>, and <c>LastName</c> for logging purposes.
 /// </remarks>
