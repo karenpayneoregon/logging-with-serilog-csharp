3be94e4860e0c2ca9cfe2d2c3994e0bb5fcf56fe
Karen Payne <paynekaren@comcast.net>
2026-09-23 13:28:53 -0700

    updates

--- EF_Core3/Pages/Index.cshtml.cs
+++ EF_Core3/Pages/Index.cshtml.cs
@@ -4,23 +4,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
-namespace EF_Core3.Pages
+namespace EF_Core3.Pages;
+
+public class IndexModel(Context context) : PageModel
 {
-    public class IndexModel(Context context) : PageModel
+    public void OnGet()
     {
-        public void OnGet()
-        {
-            var contactsList = context.Contacts.ToList();
+        var contactsList = context.Contacts.ToList();
             
-            var customersList = context.Customers
-                .Include(c => c.CountryIdentifierNavigation)
-                .ToList();
+        var customersList = context.Customers
+            .Include(c => c.CountryIdentifierNavigation)
+            .ToList();
             
-            Log.Information("Retrieved {ContactsCount} " +
-                            "contacts and {CustomersCount} " +
-                            "customers from the database.", 
-                contactsList.Count, 
-                customersList.Count);
-        }
+        Log.Information("Retrieved {ContactsCount} " +
+                        "contacts and {CustomersCount} " +
+                        "customers from the database.", 
+            contactsList.Count, 
+            customersList.Count);
     }
 }
